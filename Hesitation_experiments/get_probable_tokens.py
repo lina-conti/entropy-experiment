717fbe842547bbdf8cb2a2d237ac9c39849cabae
lina-conti <linavarellaconti@gmail.com>
2022-06-17 17:28:42 +0200

    started to code get_probable_tokens (there is a bug)

--- Hesitation_experiments/get_probable_tokens.py
+++ Hesitation_experiments/get_probable_tokens.py
@@ -1,17 +1,24 @@
-from utils import *
+'''
+Creates and saves a matrix of size number of tokens x n
+with the id of the n most probable tokens for each decision
+And another matrix of same shape with the log probability for each of them
+'''
+N = 3
 
+import sys
+sys.path.insert(0, '/home/lina/Desktop/Stage/Experiences/code')
+from utils import *
 
 model = load_model("/home/lina/Desktop/Stage/transformer_wmt15_fr2en/transformer_wmt15_fr2en.yaml")
 max_output_length = load_config("/home/lina/Desktop/Stage/transformer_wmt15_fr2en/transformer_wmt15_fr2en.yaml")["training"]["max_output_length"]
 bos_index = model.bos_index
 eos_index = model.eos_index
 
-f = open("/home/lina/Desktop/Stage/Modified_data/X-a-fini.gold.bpe.fra")
+f = open("/home/lina/Desktop/Stage/Modified_data/newstest2014.clean.bpe.fra")
 
-# not saving probability distributions because they take up too much space
-#res_df = pd.DataFrame(columns=("token_position", "sentence_length", "log_probas", "entropy"))
-res_df = pd.DataFrame(columns=("token_position", "sentence_length", "entropy"))
-token_counter = 0
+# array of size number of tokens x n
+res_tokens = []
+res_lprob = []
 for s, sentence in enumerate(f):
 
     print("translating sentence ", s)
@@ -23,7 +30,6 @@ for s, sentence in enumerate(f):
     ys = encoder_output.new_full([1, 1], bos_index, dtype=torch.long)
     trg_mask = src_mask.new_ones([1, 1, 1])
 
-    bos_id = token_counter
     for i in range(max_output_length):
 
         model.eval()
@@ -41,25 +47,31 @@ for s, sentence in enumerate(f):
 
         logits = logits[:, -1]
         log_probas = log_softmax(logits)
-        probas = softmax(logits)
 
         max_value, pred_trg_token = torch.max(logits, dim=1)
         pred_trg_token = pred_trg_token.data.unsqueeze(-1)
-        res_df.loc[token_counter] = pd.Series({"token_position": i,
-                    "sentence_length": -1,
-                    #"log_probas": log_probas[0].detach().cpu().numpy(),
-                    "entropy": entropy(probas[0].detach().cpu().numpy())
-                    })
-
         ys = torch.cat([ys, IntTensor([[pred_trg_token]])], dim=1)
+        print(to_tokens(pred_trg_token, model))
 
-        token_counter += 1
-        # print(ys)
+        """
+        n_best_tokens = []
+        n_best_probabilities = []
+        for j in range(N):
+            n_best_tokens.append(int(log_probas.argmax()))
+            n_best_probabilities.append(float(log_probas.max()))
+            log_probas[0][log_probas.argmax()] = float('-inf')
+        res_tokens.append(n_best_tokens)
+        res_lprob.append(n_best_probabilities)
+        """
         if(pred_trg_token == eos_index):
             break
+    x = input()
+    if x == 'stop':
+        break
 
-    res_df["sentence_length"][bos_id:bos_id + i + 1] = i
+print(res_tokens)
+print(res_lprob)
 
 f.close()
 
-res_df.to_pickle('/home/lina/Desktop/Stage/Experience_entropie/results/entropies_X-a-fini.pickle')
+#res_df.to_pickle('/home/lina/Desktop/Stage/Experience_entropie/results/entropies_X-a-fini.pickle')
