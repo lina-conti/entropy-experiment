625bb2dea12c057df311b1aca9f2f80db2602911
lina-conti <linavarellaconti@gmail.com>
2022-07-11 16:05:24 +0200

    test

--- Entropy_experiments/mistake_analysis.py
+++ Entropy_experiments/mistake_analysis.py
@@ -2,6 +2,8 @@ import sys
 sys.path.insert(0, '/home/lina/Desktop/Stage/Experiences/code')
 from utils import *
 
+itertools.zip()
+
 def predict_token(encoder_output: Tensor, history: Tensor, src_mask: Tensor,
                                         trg_mask:Tensor, model: Model) -> int:
     model.eval()
@@ -61,11 +63,15 @@ def mistake_stats(src_corpus: str, trg_corpus: str, pred_corpus: str, model: Mod
     bos_index = model.bos_index
     only_gold = 0
     only_predicted = 0
-    df = pd.DataFrame([[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], ["-", "-", "-", 0, 0, "-"]],
-                    index=["correct predictions", "incorrect predictions", "standard deviation"],
+    df = pd.DataFrame([[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]],
+                    index=["correct_predictions", "incorrect_predictions"],
                     columns=["gold history", "greedy history", "beam search history",
                         "1 greedy mistake", "1 beam search mistake", "last token mistake"])
 
+    one_greedy_correct = np.zeros(10)
+    one_greedy_incorrect = np.zeros(10)
+    one_bs_correct = np.zeros(10)
+    one_bs_incorrect = np.zeros(10)
     with open(src_corpus) as f_src:
         with open(trg_corpus) as f_trg:
             with open(pred_corpus) as f_pred:
@@ -86,9 +92,6 @@ def mistake_stats(src_corpus: str, trg_corpus: str, pred_corpus: str, model: Mod
                                                     [1, 1], bos_index, dtype=torch.long)
                         trg_mask = src_mask.new_ones([1, 1, 1])
 
-                        one_mistake_correct = np.zeros(10)
-                        one_mistake_incorrect = np.zeros(10)
-
                         for gold_trg_token, bs_trg_token in itertools.zip_longest(gold_target, pred_target):
                             if gold_trg_token == None:
                                 break
@@ -97,17 +100,17 @@ def mistake_stats(src_corpus: str, trg_corpus: str, pred_corpus: str, model: Mod
                             pred_token_forced = predict_token(encoder_output,
                                 ys_gold, src_mask, trg_mask, model)
                             if pred_token_forced != gold_trg_token:
-                                df.loc["incorrect predictions","gold history"] += 1
+                                df.loc["incorrect_predictions","gold history"] += 1
                             else:
-                                df.loc["correct predictions","gold history"] += 1
+                                df.loc["correct_predictions","gold history"] += 1
 
                             # greedy decoding
                             pred_token_greedy = predict_token(encoder_output,
                                 ys, src_mask, trg_mask, model)
                             if pred_token_greedy != gold_trg_token:
-                                df.loc["incorrect predictions", "greedy history"] += 1
+                                df.loc["incorrect_predictions", "greedy history"] += 1
                             else:
-                                df.loc["correct predictions", "greedy history"] += 1
+                                df.loc["correct_predictions", "greedy history"] += 1
 
                             if pred_token_forced != pred_token_greedy:
                                 if pred_token_forced == gold_trg_token:
@@ -119,39 +122,66 @@ def mistake_stats(src_corpus: str, trg_corpus: str, pred_corpus: str, model: Mod
                             pred_token_last = predict_token(encoder_output,
                                 ys_last, src_mask, trg_mask, model)
                             if pred_token_last != gold_trg_token:
-                                df.loc["incorrect predictions", "last token mistake"] += 1
+                                df.loc["incorrect_predictions", "last token mistake"] += 1
                             else:
-                                df.loc["correct predictions", "last token mistake"] += 1
+                                df.loc["correct_predictions", "last token mistake"] += 1
 
-                            # history with only one predicted mistake
+                            # history with only one greedy mistake
                             for i in range(10):
-
-
-                            pred_token_1mistake = predict_token(encoder_output,
-                                ys_1mistake, src_mask, trg_mask, model)
-                            if pred_token_1mistake != gold_trg_token:
-                                one_mistake_incorrect[i] += 1
-                                df.loc["incorrect predictions", "1 greedy mistake"] += 1
-                            else:
-                                one_mistake_correct[i] += 1
-                                df.loc["correct predictions", "1 greedy mistake"] += 1
+                                ys_1mistake = history_one_mistake(ys_gold, ys)
+                                pred_token_1mistake = predict_token(encoder_output,
+                                    ys_1mistake, src_mask, trg_mask, model)
+                                if pred_token_1mistake != gold_trg_token:
+                                    one_greedy_incorrect[i] += 1
+                                else:
+                                    one_greedy_correct[i] += 1
 
                             # beam search decoding
-                            """pred_token_bs = predict_token(encoder_output,
-                                ys_bs, src_mask, trg_mask, model)
-                            if pred_token_bs != gold_trg_token:
-                                df.loc["1 predicted mistake","incorrect predictions"] += 1
-                            else:
-                                df.loc["1 predicted mistake","correct predictions"] += 1"""
+                            if ys_bs != None:
+                                pred_token_bs = predict_token(encoder_output,
+                                    ys_bs, src_mask, trg_mask, model)
+                                if pred_token_bs != gold_trg_token:
+                                    df.loc["incorrect_predictions", "beam search history"] += 1
+                                else:
+                                    df.loc["correct_predictions", "beam search history"] += 1
+
+                            # history with only one beam search mistake
+                            if ys_bs != None:
+                                for i in range(10):
+                                    ys_1mistake = history_one_mistake(ys_gold, ys_bs)
+                                    pred_token_1mistake = predict_token(encoder_output,
+                                        ys_1mistake, src_mask, trg_mask, model)
+                                    if pred_token_1mistake != gold_trg_token:
+                                        one_bs_incorrect[i] += 1
+                                    else:
+                                        one_bs_correct[i] += 1
 
                             ys = torch.cat([ys, IntTensor([[pred_token_greedy]])], dim=1)
                             ys_gold = torch.cat([ys_gold, IntTensor([[gold_trg_token]])], dim=1)
                             pred_wrong_token = predict_wrong_token(gold_trg_token,
                                 encoder_output, ys, src_mask, trg_mask, model)
                             ys_last = torch.cat([ys_gold, IntTensor([[pred_wrong_token]])], dim=1)
-                            ys_1mistake = history_one_mistake(ys_gold, ys)
                             if bs_trg_token:
                                 ys_bs = torch.cat([ys_bs, IntTensor([[bs_trg_token]])], dim=1)
+                            else:
+                                ys_bs = None
+
+    df.loc["incorrect_predictions", "1 greedy mistake"] = one_greedy_incorrect.mean()
+    df.loc["correct_predictions", "1 greedy mistake"] = one_greedy_correct.mean()
+    df.loc["incorrect_predictions", "1 beam search mistake"] = one_bs_incorrect.mean()
+    df.loc["correct_predictions", "1 beam search mistake"] = one_bs_correct.mean()
+
+    df = df.append(df.apply(
+        lambda x: x.correct_predictions * 100 / (x.correct_predictions + x.incorrect_predictions),
+        axis=0).rename("percentage correct"))
+    df = pd.concat([df, pd.Series(["-", "-", "-", "-", "-", "-"], name="standard deviation")], ignore_index=True)
+    print(df)
+    df.loc["standard deviation", "1 greedy mistake"] = one_greedy_correct.std() \
+        * 100 / (df.loc["correct_predictions", "1 greedy mistake"] + \
+        df.loc["incorrect_predictions", "1 greedy mistake"])
+    df.loc["standard deviation", "1 beam search mistake"] = one_bs_correct.std() \
+        * 100 / (df.loc["correct_predictions", "1 beam search mistake"] + \
+        df.loc["incorrect_predictions", "1 beam search mistake"])
 
     return df, only_gold, only_predicted
 
@@ -166,7 +196,8 @@ if __name__ == "__main__":
     parser.add_argument("target_corpus", help="path to the target corpus")
     parser.add_argument("predicted_corpus", help="path to the corpus translated \
     by the system using beam search")
-    args = parser.parse_args()
+    #args = parser.parse_args()
+    args = parser.parse_args("/home/lina/Desktop/Stage/Modified_data/X-a-fini.small.bpe.fra /home/lina/Desktop/Stage/Modified_data/X-a-fini.small.bpe.eng /home/lina/Desktop/Stage/Experiences/results/Hesitation_experiments/tests/X-a-fini.small.bpe.eng".split())
 
     model = load_model("/home/lina/Desktop/Stage/transformer_wmt15_fr2en/transformer_wmt15_fr2en.yaml")
     max_output_length = load_config("/home/lina/Desktop/Stage/transformer_wmt15_fr2en/transformer_wmt15_fr2en.yaml")["training"]["max_output_length"]
